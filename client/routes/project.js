6cc1a5158878b52b243a544e1c48683db06542ae
Gurjotsf <gurjotsf42005@gmail.com>
2026-06-17 17:39:14 +0530

    Cleanup: Final removal of backend files from frontend repo

--- client/routes/project.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllProjects } = require('../controllers/Project');
-
-router.get('/', getAllProjects);
-
-module.exports = router;
--- client/routes/siteSettings.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getSiteSettings, updateSiteSettings } = require('../controllers/siteSettingsController');
-const { adminAuth } = require('../middleware/authMiddleware');
-
-router.get('/', getSiteSettings);
-router.put('/', adminAuth, updateSiteSettings);
-
-module.exports = router;
