75d6c3afb339b90eb778bb94c6710d9bea7f8d3a
Gurjotsf <gurjotsf42005@gmail.com>
2026-06-17 14:58:26 +0530

    Fix file naming issues with spaces

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,9 +5,9 @@ import "./styles/global.css";
 import "./styles/admin.css";
 import "./styles/premium.css";
 import "./styles/premiumroyal.css"
-import "./styles/Navmobilemenu fix .css"
-import "./styles/Zzfinalfixes .css"
-import "./styles/Navbarroyal .css"
+import "./styles/Navmobilemenufix .css"
+import "./styles/Zzfinalfixes.css"
+import "./styles/Navbarroyal.css"
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
